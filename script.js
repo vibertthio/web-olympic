43375618bef4dbf53c214ed79e3124afa411f0da
Vibert Thio <vibertthio@gmail.com>
2019-12-03 20:35:05 -0500

    add some web design

---
+++ script.js
@@ -0,0 +1,16 @@
+/* If you're feeling fancy you can add interactivity
+    to your site with Javascript */
+
+getRecords()
+
+
+
+async function getRecords() {
+	const response = await fetch("https://whack-mole.glitch.me/leaders")
+	const records = await response.json()
+	records.forEach(user => {
+		const li = document.createElement("li")
+		li.appendChild(document.createTextNode(user.name + ": " + user.score));
+		document.getElementById('users').appendChild(li)
+	});
+}
