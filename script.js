6f868059b650de842a8357066e507a686cbdda7f
Vibert Thio <vibertthio@gmail.com>
2019-12-09 18:42:04 -0500

    add loading hint

--- script.js
+++ script.js
@@ -3,11 +3,10 @@
 
 getRecords()
 
-
-
 async function getRecords() {
 	const response = await fetch("https://whack-mole.glitch.me/leaders")
 	const records = await response.json()
+	document.getElementById('leader-loading').style.display = 'none'
 	records.forEach(user => {
 		const li = document.createElement("li")
 		li.appendChild(document.createTextNode(user.name + ": " + user.score));
